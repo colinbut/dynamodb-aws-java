d93f31bbe5faed4e5e048afc6ef13db5ebd4f4d2
Colin But <colinbut@users.noreply.github.com>
2018-08-26 09:49:47 +0100

    Tidy up the generated source files

--- src/main/java/com/mycompany/aws/dynamodb/java/App.java
+++ src/main/java/com/mycompany/aws/dynamodb/java/App.java
@@ -1,11 +1,6 @@
 package com.mycompany.aws.dynamodb.java;
 
-/**
- * Hello world!
- *
- */
-public class App 
-{
+public class App {
     public static void main( String[] args )
     {
         System.out.println( "Hello World!" );
